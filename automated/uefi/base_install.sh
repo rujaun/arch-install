097c3326ef1ee5997a24b3f17b0650274b581016
Rujaun Fourie <rujaun@gmail.com>
2020-10-28 16:00:23 +0200

    Initial UEFI automated install script

---
+++ automated/uefi/base_install.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+echo -n "Disk name:"
+
+read DISK
+
+echo -n "Do you require a swap partition? (Y/N)"
+
+read SWAP
+SWAP_SIZE=0
+
+if [ "$SWAP" = "Y" ]; then
+	echo -n "SWAP size in G:"
+	read SWAP_SIZE
+fi
+
+echo -n "Hostname:"
+
+read HOST
+
+echo -n "ROOT Password:"
+
+read ROOT_PASSWORD
+
+echo -n "User name (lowercase):"
+
+read USERNAME
+
+echo -n "User Password:"
+
+read USER_PASSWORD
+
+echo -n "GPU (AMD | Intel | Nvidia):"
+
+read GPU
+
+echo -n "CPU (AMD | Intel):"
+
+read CPU
+
+# Create Partitions:
+DISK="/dev/${DISK}"
+BOOT_PARTITION="${DISK}1"
+ROOT_PARTITION="${DISK}2"
+SWAP_PARTITION=""
+
+if [ "$SWAP" = "Y" ]; then
+	SWAP_PARTITION="${DISK}2"
+	ROOT_PARTITION="${DISK}3"
+fi
+
+echo -n "Creating GPT partition table"
+
+parted -S "$DISK" mklabel gpt
+
+echo -n "Creating UEFI Boot partition:"
+parted -s optimal "$DISK" mkpart "EFI system partition" fat32 2MiB 512MiB
+parted -s set 1 esp on
+
+
+if [ "$SWAP" = "Y" ]; then
+	echo -n "Creating SWAP partition:"
+	parted -s optimal "$DISK" mkpart "swap partition" linux-swap 514MiB 8706MiB
+fi
+
+echo -n "Creating root partition:"
+if [ "$SWAP" = "Y" ]; then
+	parted -s optimal "$DISK" mkpart "root partition" ext4 8708MiB 100%
+else
+	parted -s optimal "$DISK" mkpart "root partition" ext4 514MiB 100%
+fi
+
+# Format partitions:
+echo -n "Formatting EFI partition:"
+mkfs.fat -F32 "${BOOT_PARTITION}"
+
+if [ "$SWAP" = "Y" ]; then
+	echo -n "Making SWAP partition..."
+	mkswap "${SWAP_PARTITION}"
+fi
+
+echo -n "Formatting root partition"
+mkfs.ext4 "${ROOT_PARTITION}"
+
+# Update repos
+pacman -Syy --noconfirm
+
+echo -n "Installing and running reflector to update mirrors..."
+pacman -S reflector --noconfirm
+reflector -c "ZA" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+# Mount root partition
+echo -n "Mounting root partition..."
+mount "${ROOT_PARTITION}" /mnt
+
+# Install base system
+echo -n "Installing base system..."
+pacstrap /mnt base base-devel linux linux-firmware linux-headers util-linux amd-ucode vim
+
+# Generate fstab
+echo -n "Generating fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+#Preparing chroot script handoff
+echo -n "Preparing chroot script handoff"
+cp ./chroot_install.sh /mnt
+
+echo -n "Entering chroot"
+arch-chroot /mnt ./chroot_install.sh "$DISK" "$SWAP" "$BOOT_PARTITION" "$ROOT_PASSWORD" "$USERNAME" "$USER_PASSWORD" "$HOST" "$GPU" "$CPU"
+
+echo -n "Removing chroot_install.sh"
+rm /mnt/chroot_install.sh
+
+echo -n "Unmounting root partition and reboot"
+umount -R /mnt
+systemctl reboot
---
+++ automated/uefi/chroot_install.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+DISK="$1"
+SWAP="$2"
+BOOT_PARTITION="$3"
+ROOT_PASSWORD="$4"
+USERNAME="$5"
+USER_PASSWORD="$6"
+HOST="$7"
+GPU="$8"
+CPU="$9"
+
+echo -n "Setting timezone..."
+timedatectl set-timezone Africa/Johannesburg
+
+echo -n "Updating hardware clock..."
+timedatectl set-local-rtc 1 --adjust-system-clock
+
+echo -n "Setting locale..."
+echo en_ZA.UTF-8 UTF-8 > /etc/locale.gen
+echo LANG=en_ZA.UTF-8 > /etc/locale.conf
+export LANG=en_ZA.UTF-8
+locale-gen
+
+echo -n "Setting hostname and configuring hosts..."
+echo "$HOST" > /etc/hostname
+echo "127.0.0.1			localhost" >> /etc/hosts
+echo "::1				localhost" >> /etc/hosts
+echo "127.0.1.1			$HOST" >> /etc/hosts
+
+echo -n "Setting root password..."
+echo "root:$ROOT_PASSWORD" | chpasswd
+
+echo -n "Install GRUB and configuring bootloader..."
+pacman -S grub efibootmgr --noconfirm
+mkdir /boot/efi
+mount "$BOOT_PARTITION" /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo -n "Setting up local user account and installing sudo..."
+pacman -S sudo --noconfirm
+useradd --create-home "$USERNAME"
+echo "$USERNAME:$USER_PASSWORD" | chpasswd
+usermod --append --groups wheel "$USERNAME"
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo -n "Install and enable NetworkManager..."
+pacman -S networkmanager --noconfirm
+systemctl enable NetworkManager.service
+
+echo -n "Enabling TRIM scheduling for SSDs..."
+sudo systemctl enable fstrim.timer
+
+echo -n "Install AUR (Arch User Repository) helper"
+sudo pacman -S git go --noconfirm
+
+cd ~/
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+cd ~/
+rm -rf ./yay-git
+
+echo -n "Installing Xorg and GPU drivers..."
+local multilib=`grep -n "\[multilib\]" /etc/pacman.conf | cut -f1 -d:`
+sed -i "$multilib s/^#//g" /etc/pacman.conf
+local multilib=$(( $multilib + 2 ))
+sed -i "${multilib}s/^#//g" /etc/pacman.conf
+pacman -Syu --noconfirm
+
+echo -n "Installing xorg and dkms..."
+sudo pacman -S xorg dkms --noconfirm
+
+if [ "$GPU" = "AMD" ]; then
+	echo -n "Installing AMD GPU drivers"
+	sudo pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
+fi
+
+if [ "$GPU" = "Intel" ]; then
+	echo -n "Installing Intel GPU drivers"
+	sudo pacman -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
+fi
+
+if [ "$GPU" = "Nvidia" ]; then
+	echo -n "Installing Nvidia GPU drivers"
+	sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
+fi
+
+echo -n "Installing Plasma desktop environment..."
+sudo pacman -S plasma plasma-wayland-session --noconfirm
+sudo pacman -S kdegraphics-thumbnailers kio-extras --noconfirm
+sudo pacman -S bzip2 gzip lzip xz p7zip unrar zip unzip --noconfirm
+sudo pacman -S konsole kate dolphin partitionmanager kcolorchooser krita okular vlc ark persepolis transmission-qt firefox chromium ktouch
+sudo pacman -S packagekit packagekit-qt5 appstream appstream-qt --noconfirm
+
+echo -n "Installing vim-plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -n "Installing python..."
+sudo pacman -S python python-pip --noconfirm
+
+echo -n "Installing support for NTFS and exfat drives / partitions..."
+sudo pacman -S ntfs-3g exfat-utils --noconfirm
+
+echo -n "Enabling SDDM..."
+sudo pacman -S ntfs-3g exfat-utils --noconfirm
