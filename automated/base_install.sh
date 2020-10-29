19452d5ac37028aa63466d49be1dbf4e41c77f94
Rujaun Fourie <rujaun@gmail.com>
2020-10-29 11:58:18 +0200

    Added git curl and wget to base install

--- automated/base_install.sh
+++ automated/base_install.sh
@@ -89,7 +89,7 @@ mount "${ROOT_PARTITION}" /mnt
 
 # Install base system
 echo -e "\nInstalling base system..."
-pacstrap /mnt base base-devel linux linux-firmware linux-headers util-linux vim htop
+pacstrap /mnt base base-devel linux linux-firmware linux-headers util-linux vim htop git curl wget
 
 # Generate fstab
 echo -e "\nGenerating fstab"
