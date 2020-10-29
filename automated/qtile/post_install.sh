0cca823ed2db3875e5344980e2bf082fd28ef3e2
Rujaun Fourie <rujaun@gmail.com>
2020-10-29 17:45:18 +0200

    added qtile terminal

--- automated/qtile/post_install.sh
+++ automated/qtile/post_install.sh
@@ -11,17 +11,20 @@ cp ./.xinitrc ~/.xinitrc
 
 echo -e "\nAdding startx to bash_profile"
 
-	FILE=~/.bash_profile
-	if [[ -f "$FILE" ]]; then
-		echo -e "\n$FILE exists."
-		
-		echo -e "\nWriting to bash_profile"
-		echo "[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx --vt1" > ~/.bash_profile
-	else
-		echo -e "\nCreating to bash_profile"
-		touch ~/.bash_profile
-		echo "[[ -f ~/.bashrc ]] && . ~/.bashrc" > ~/.bash_profile
+FILE=~/.bash_profile
+if [[ -f "$FILE" ]]; then
+	echo -e "\n$FILE exists."
+	
+	echo -e "\nWriting to bash_profile"
+	echo "[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx --vt1" > ~/.bash_profile
+else
+	echo -e "\nCreating to bash_profile"
+	touch ~/.bash_profile
+	echo "[[ -f ~/.bashrc ]] && . ~/.bashrc" > ~/.bash_profile
 
-		echo -e "\nWriting to bash_profile"
-		echo "[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx --vt1" >> ~/.bash_profile
-	fi
+	echo -e "\nWriting to bash_profile"
+	echo "[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx --vt1" >> ~/.bash_profile
+fi
+
+echo -e "\nCopying qtile configuration..."
+cp ./config.py ~/.config/qtile/config.py
